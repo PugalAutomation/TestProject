e2ab0ae34002510401094bad4f2dd62d93584053
Anish <anishbaksh95@gmail.com>
2023-07-12 13:10:08 +0530

    kygu

--- src/com/anish/com/TestPro2.java
+++ src/com/anish/com/TestPro2.java
@@ -2,4 +2,8 @@ package com.anish.com;
 
 public class TestPro2 {
 
+	public static void main(String[] args) {
+		
+	}
+	
 }
--- src/com/test/com/TestClass.java
+++ src/com/test/com/TestClass.java
@@ -2,4 +2,13 @@ package com.test.com;
 
 public class TestClass {
 
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		int a=0;
+	}
 }
