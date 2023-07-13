ca4aba39d136a111eebcc9769952e109f8829d79
PugazhendhiAutomation <spugazhendhi747@gmail.com>
2023-07-13 12:27:04 +0530

    hi

---
+++ src/com/test/com/findElements.java
@@ -0,0 +1,16 @@
+package com.test.com;
+
+
+
+public class findElements {
+
+	public static void main(String[] args) {
+
+		int a=89;
+		int b=90;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
